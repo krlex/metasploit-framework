e30abad081ffc1bde1badf3887de8c0f96b157b3
krlex <krle@tilda.center>
2017-10-27 12:13:05 +0200

    add install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-sudo apt install -y  rubygems libpq-dev libpcap-dev ruby-dev yard
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y  curl rubygems libpq-dev libpcap-dev ruby-dev yard
 sudo gems install bundler
 sudogpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 cd /tmp
