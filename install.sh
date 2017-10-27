04b94bcf3b93813146433e10e22dce2c52c90f20
krlex <krle@tilda.center>
2017-10-27 12:26:26 +0200

    add install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y  curl rubygems libpq-dev libpcap-dev ruby-dev yard
 sudo gem install bundler
-sudogpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 cd /tmp
 sudo curl -sSL https://get.rvm.io -o rvm.sh
 sudo cat /tmp/rvm.sh | bash -s stable --rails
