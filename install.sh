884f57366fd794756cace0effbd73f27c1187f06
krlex <krle@tilda.center>
2017-10-27 14:31:09 +0200

    fix spell

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo gem install bundler
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 cd /tmp
 sudo curl -sSL https://get.rvm.io -o rvm.sh
-sudo cat /tmp/rvm.sh | bash -s stable --rails
+cat /tmp/rvm.sh | bash -s stable --rails
 source /usr/local/rvm/scripts/rvm
 rvm install "ruby-2.4.2"
 bundler install
